288d8d47a45afbdf9ece32b4ae61cd622f175492
Lui Kohl <lui.kohl@gmail.com>
2018-08-04 18:28:49 +0200

    initializing react app

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const list = [
+  {
+    title: 'Pink Romaguera',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Ms. Mathew Nicolas',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: 'John Doe',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Pete, Sandra',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase ().includes (searchTerm.toLowerCase ());
+
+class App extends Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind (this);
+    this.onDismiss = this.onDismiss.bind (this);
+  }
+
+  onSearchChange (event) {
+    this.setState ({searchTerm: event.target.value});
+  }
+
+  onDismiss (id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter (isNotId);
+    this.setState ({list: updatedList});
+  }
+
+  render () {
+    const {searchTerm, list} = this.state;
+    return (
+      <div className="App">
+        <form>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {this.state.list.filter (isSearched (searchTerm)).map (item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            {' '}
+            <span>{item.num_comments}</span>
+            {' '}
+            <span>{item.points}</span>
+            {' '}
+            <span>
+              <button
+                onClick={() => this.onDismiss (item.objectID)}
+                type="button"
+              >
+                {' '}Dismiss
+              </button>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
