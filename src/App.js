ccf02488da488817b7b341da1bfacabf6931d89c
Lui Kohl <lui.kohl@gmail.com>
2018-08-07 19:47:02 +0200

    completed the proptypes for now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -176,10 +177,41 @@ const Table = ({list, pattern, onDismiss}) => (
   </div>
 );
 
-function Button({onClick, className = '', children}) {
-  return <button onClick={onClick} className={className} type="button" />;
-}
+const Button = ({onClick, className, children}) => (
+  <button onClick={onClick} className={className} type="button">
+    {' '}{children}
+  </button>
+);
+
+Button.defaultProps = {
+  className: '',
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Table.propTypes = {
+  list: PropTypes.arrayOf (
+    PropTypes.shape ({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func,
+};
+
+Search.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+  children: PropTypes.node.isRequired,
+};
 
 export default App;
-
 export {Button, Search, Table};
